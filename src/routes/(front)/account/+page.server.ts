7124a3ac881ba56587ba6ae3aea89dd0058134bf
pawelwos <pawelwos@gmail.com>
2023-05-16 22:56:31 +0200

    Account page tweaks

--- src/routes/(front)/account/+page.server.ts
+++ src/routes/(front)/account/+page.server.ts
@@ -3,9 +3,9 @@ import Stripe from 'stripe';
 import { SECRET_STRIPE_KEY } from '$env/static/private'
 
 export const actions = {
-	pay: async ({request, locals, cookies}) => {
+	pay: async ({ request, locals, cookies }) => {
 
-		const body = Object.fromEntries(await request.formData() )
+		const body = Object.fromEntries(await request.formData())
 
 		// init Stripe SDK
 		const stripe = new Stripe(SECRET_STRIPE_KEY);
@@ -17,21 +17,34 @@ export const actions = {
 	}
 }
 
-export const load = async ({locals, params, cookies }) => {
-	if(!locals.pb.authStore.isValid) {
+export const load = async ({ locals, params, cookies }) => {
+	if (!locals.pb.authStore.isValid) {
 		throw redirect(303, '/login')
 	}
-  const result = await locals.pb.collection('options').getOne('bi3g03tppvmir2y');
+	const result = await locals.pb.collection('options').getOne('bi3g03tppvmir2y');
 	const shipping = structuredClone(result);
 
 
 	const records = await locals.pb.collection('orders').getList(1, 50, {
-			filter: 'user = "'+locals.pb?.authStore?.model?.id+'"',
-			sort: '-created',
-			expand: 'payment'
+		filter: 'user = "' + locals.pb?.authStore?.model?.id + '"',
+		sort: '-created',
+		expand: 'payment'
 	});
+	let carts = []
+	records.items.map(record => {
+		const cart = record.cart
+		const products = cart.split("\n")
+		carts[record.id] = []
+		products.map(prod => {
+			const columns = prod.split(" | ")
+			carts[record.id].push(columns)
+		})
+
+	})
+
 	return {
 		orders: structuredClone(records),
-    shipping
+		carts,
+		shipping
 	}
 }
