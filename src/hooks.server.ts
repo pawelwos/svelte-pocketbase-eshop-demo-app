85b3ee98b2de1e5a900e1c39d5426daeb1e791d6
Pawel Wos <pawelwos@gmail.com>
2023-05-24 18:02:36 +0200

    Svelte hook tweaked

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,7 +7,7 @@ export async function handle({ event, resolve }) {
     event.locals.pb = new PocketBase(PUBLIC_PB_URL);
 
     // load the store data from the request cookie string
-    const ev = event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
+    await event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
     try {
         // get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
         event.locals.pb.authStore.isValid && await event.locals.pb.collection('users').authRefresh();
